afcc2eebf663b10c02fe7a7f1be93701aa4419b0
Kamil Gizinski <alvix360@gmail.com>
2021-05-22 18:38:44 +0200

    Pierwszy commit - dodanie wszystkich nowych plików.

---
+++ Kalkulator/Kalkulator.cpp
@@ -0,0 +1,13 @@
+#include "Kalkulator.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    //WinformCDemo is your project name
+    Kalkulator::Kalkulator form;
+    Application::Run(% form);
+}
