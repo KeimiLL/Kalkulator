34ae3655a63ccf8576cfd24d1fa8a12dde5f0a57
Domin8668 <dominik.sigulski@protonmail.com>
2021-05-22 19:08:44 +0200

    Wypisywanie do konsoli w Kalkulator.cpp.

--- Kalkulator/Kalkulator.cpp
+++ Kalkulator/Kalkulator.cpp
@@ -1,10 +1,12 @@
 #include "Kalkulator.h"
+#include <iostream>
 
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThreadAttribute]
 void Main(array<String^>^ args) {
+    std::cout << "Wypisujemy do konsoli";
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
     //WinformCDemo is your project name
